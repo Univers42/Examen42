12c4a8da713e4b53e7050488dd7a2a53fdf1856d
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-15 14:31:39 +0200

    Update str_capitalizer.c
    answer that work and best to understand and modularize

--- my_answers/exam_rank_02/level_3/str_capitalizer/str_capitalizer.c
+++ my_answers/exam_rank_02/level_3/str_capitalizer/str_capitalizer.c
@@ -1,48 +1,43 @@
 #include <unistd.h>
 
-void str_capitalizer(char *str)
+// Character classification macros
+#define IS_LETTER(c) (((c & ~32) >= 'A') && ((c & ~32) <= 'Z'))
+#define IS_SPACE(c) ((c) == ' ' || (c) == '\t')
+
+// Case conversion macros
+#define TO_UPPER(c) ((c) & ~32)
+#define TO_LOWER(c) ((c) | 32)
+
+// Output macros
+#define WRITE_CHAR(c) write(1, &(c), 1)
+#define WRITE_NEWLINE() write(1, "\n", 1)
+
+void rstr_capitalizer(char *str)
 {
-    int i = 0;
-    int new_word = 1; // Track the start of a word
-
-    while (str[i])
+    while (*str)
     {
-        // Check if character is a letter using bitwise operation
-        char c = str[i];
-        int is_letter = ((c & ~32) - 'A') < 26;
-
-        if (is_letter)
+        if (IS_LETTER(*str))
         {
-            if (new_word)
-                c &= ~32; // Convert to uppercase (clear bit 5)
+            if (*(str + 1) == '\0' || IS_SPACE(*(str + 1)))
+                *str = TO_UPPER(*str);
             else
-                c |= 32;  // Convert to lowercase (set bit 5)
+                *str = TO_LOWER(*str);
         }
-
-        write(1, &c, 1);
-        
-        // Update new_word flag for next iteration
-        // Current char is delimiter -> next char starts a new word
-        new_word = (str[i] == ' ' || str[i] == '\t');
-        i++;
+        WRITE_CHAR(*str++);
     }
 }
 
 int main(int argc, char **argv)
 {
     if (argc == 1)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
+        return(WRITE_NEWLINE(), 0);
     
     int i = 1;
     while (i < argc)
     {
-        str_capitalizer(argv[i]);
-        write(1, "\n", 1);
+        rstr_capitalizer(argv[i]);
+        WRITE_NEWLINE();
         i++;
     }
-    
     return 0;
 }
