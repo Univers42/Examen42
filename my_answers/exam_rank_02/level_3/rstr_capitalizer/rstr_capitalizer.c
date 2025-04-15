e7c376ead7913a86b36371ab701cdb9f8ae72ae3
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-15 14:32:28 +0200

    Update rstr_capitalizer.c
    update

--- my_answers/exam_rank_02/level_3/rstr_capitalizer/rstr_capitalizer.c
+++ my_answers/exam_rank_02/level_3/rstr_capitalizer/rstr_capitalizer.c
@@ -1,64 +1,43 @@
 #include <unistd.h>
 
-// Checks if a character is a letter using bitwise operations
-int is_letter(char c)
-{
-    // Convert to uppercase and check if it's in [A-Z] range
-    return ((c & ~32) - 'A') < 26;
-}
+// Character classification macros
+#define IS_LETTER(c) (((c & ~32) >= 'A') && ((c & ~32) <= 'Z'))
+#define IS_SPACE(c) ((c) == ' ' || (c) == '\t')
 
-// Checks if a character is a delimiter using bitwise operations
-int is_delimiter(char c)
-{
-    // Simple and efficient check without shift-count warnings
-    return (c == ' ' || c == '\t' || c == '\0');
-}
+// Case conversion macros
+#define TO_UPPER(c) ((c) & ~32)
+#define TO_LOWER(c) ((c) | 32)
+
+// Output macros
+#define WRITE_CHAR(c) write(1, &(c), 1)
+#define WRITE_NEWLINE() write(1, "\n", 1)
 
 void rstr_capitalizer(char *str)
 {
-    int i = 0;
-    
-    while (str[i])
+    while (*str)
     {
-        // Look ahead to determine if this is the last letter of a word
-        if (is_letter(str[i]) && (is_delimiter(str[i + 1])))
+        if (IS_LETTER(*str))
         {
-            // Last letter - convert to uppercase by clearing bit 5
-            char c = str[i] & ~32;
-            write(1, &c, 1);
+            if (*(str + 1) == '\0' || IS_SPACE(*(str + 1)))
+                *str = TO_UPPER(*str);
+            else
+                *str = TO_LOWER(*str);
         }
-        else if (is_letter(str[i]))
-        {
-            // Not the last letter - convert to lowercase by setting bit 5
-            char c = str[i] | 32;
-            write(1, &c, 1);
-        }
-        else
-        {
-            // Not a letter, write as-is
-            write(1, &str[i], 1);
-        }
-        i++;
+        WRITE_CHAR(*str++);
     }
 }
 
 int main(int argc, char **argv)
 {
-    int i;
-    
     if (argc == 1)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
+        return(WRITE_NEWLINE(), 0);
     
-    i = 1;
+    int i = 1;
     while (i < argc)
     {
         rstr_capitalizer(argv[i]);
-        write(1, "\n", 1);
+        WRITE_NEWLINE();
         i++;
     }
-    
     return 0;
 }
