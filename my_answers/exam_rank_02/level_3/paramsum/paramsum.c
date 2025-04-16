9e270857736ec55c9feaacc4ed4db83b4c3e5dc9
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-16 19:36:00 +0200

    Update paramsum.c
    update

--- my_answers/exam_rank_02/level_3/paramsum/paramsum.c
+++ my_answers/exam_rank_02/level_3/paramsum/paramsum.c
@@ -5,9 +5,9 @@ void putnbr(int n)
     char c;
     
     if (n > 9)
-        putnbr(n / 10);   // Normal division remains for recursion
-    c = (n & 0b1111) + '0';  // Faster modulo (n % 10) using bitwise mask
-    write(1, &c, 1);
+        putnbr(n / 10);
+    c = (n % 10) + '0';
+   write(1, &c, 1);
 }
 
 int main(int argc, char **argv)
