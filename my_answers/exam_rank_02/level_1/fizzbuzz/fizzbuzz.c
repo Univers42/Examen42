e1da7cb3b7c1af54f490d26e288dd908d7bbc113
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-17 15:45:16 +0000

    Update fizzbuzz.c
    optimizations of my prior solution.

--- my_answers/exam_rank_02/level_1/fizzbuzz/fizzbuzz.c
+++ my_answers/exam_rank_02/level_1/fizzbuzz/fizzbuzz.c
@@ -1,73 +1,57 @@
 #include <unistd.h>
 
-/**
- * The mathematical rule to check if a number is a multiple of another number is simple: a number
- * `a` is a multiple of another number `b` if the remainder of `a % b` is zero.
- */
-void ft_putstr(char *str)
+# define FIZZ() (write(1,"fizz",4))
+# define BUZZ() (write(1,"buzz",4))
+# define FIZZBUZZ() (FIZZ(), BUZZ())
+# define NEW_LINE() (write(1,"\n",1))
+# define PUT_NBR(n) ft_putnbr_macro(n)
+
+// Helper for number printing
+static inline void ft_putnbr_macro(int n)
 {
-    while (*str)
-        write(1, str++, 1);
+	char c;
+	if (n > 9)
+		ft_putnbr_macro(n / 10);
+	c = (n % 10) + '0';
+	write(1, &c, 1);	
 }
 
-void ft_putnbr(int n)
-{
-    char c;
+// WRAPPERS OF MACROS
 
-    if (n >= 10)
-        ft_putnbr(n/10);
-    c = (n % 10 ) + '0';
-    write(1, &c, 1);
-}
+void 	ft_putnbr(int n)	{PUT_NBR(n); NEW_LINE();}
+void 	ft_fizz(int n) 		{(void) n; FIZZ(); NEW_LINE();}
+void	ft_buzz(int n) 		{(void)n; BUZZ(); NEW_LINE();}
+void 	ft_fizzbuzz(int n)	{(void)n; FIZZBUZZ(); NEW_LINE();}
+void	fizz_buzz(void);
 
-void print_number(int num)
-{
-    ft_putnbr(num);
-    ft_putstr("\n");
-}
+typedef void (*state_handler)(int);
 
-void print_fizz(int num)
-{
-    ft_putstr("fizz\n");
-}
-
-void print_buzz(int num)
-{
-    ft_putstr("buzz\n");
-}
-
-void print_fizzbuzz(int num)
-{
-    ft_putstr("fizzbuzz\n");
-}
-
-void fizzbuzz(void)
-{
-    typedef void (*state_handler)(int);
-    state_handler state_handlers[4] = 
-    {
-        print_number,
-        print_fi,
-	print_buzz,
-        print_fizzbuzz
-    };
-    int i = 1; // Fixed: initialized i to 1
-    int state;
-    
-    while (i <= 100)
-    {
-        state = 0;
-        if (i % 3 == 0)
-            state |= 1;
-        if (i % 5 == 0)
-            state |= 2;
-        state_handlers[state](i);
-        i++;
-    }
-}
-
+//totally set
 int main(void)
 {
-    fizzbuzz();
-    return (0);
+		fizz_buzz();
 }
+
+void fizz_buzz(void)
+{
+	state_handler state_handlers[4] = 
+	{
+		ft_putnbr,
+		ft_fizz,
+		ft_buzz,
+		ft_fizzbuzz
+	};
+	int i = 1;
+	int state;
+	while(i <= 100)
+	{
+		state = 0;
+		if (i % 3 == 0)
+			state |= 1;
+		if (i % 5 == 0)
+			state |= 2;
+		state_handlers[state](i);
+		i++;
+	}
+}
+
