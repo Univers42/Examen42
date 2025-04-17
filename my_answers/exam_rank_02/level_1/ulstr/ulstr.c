149c44faf7abeb04fe6cbafeb769db8859804412
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-17 19:06:41 +0000

    Update ulstr.c
    update

--- my_answers/exam_rank_02/level_1/ulstr/ulstr.c
+++ my_answers/exam_rank_02/level_1/ulstr/ulstr.c
@@ -1,27 +1,20 @@
 #include <unistd.h>
+# define NEWLINE() (write(1,"\n",1))
+# define TOGGLE(c) (c ^= 0x20)
 
 int main(int argc, char **argv)
 {
 	char *str;
-	char c;
-
-	if (argc != 2)
-		return (write(1, "\n", 1), 0);
-	str = argv[1];
-	while (*str)
+	char ch;
+	if(argc == 2)
 	{
-		c = *str;
-		if (c & 0x40)
+		str = argv[1];
+		while (*str)
 		{
-			if ((c >= 'A' && c <= 'Z') || (c >= 'a' && c <= 'z'))
-			{
-				//toggle case with XOR - The most efficient way to flip case
-				c ^= 0x20;
-			}
+			ch = TOGGLE(*str);
+			write(1, &ch, 1);
+			str++;
 		}
-		write(1, &c, 1);
-		str++;	
 	}
-	write(1, "\n", 1);
-	return (0);
+	return (NEWLINE(),1);
 }
