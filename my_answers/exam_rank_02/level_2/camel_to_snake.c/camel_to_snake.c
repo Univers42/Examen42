3c96b58390c21ab302bf9d91ed53f2ed6ecdb275
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-17 22:29:47 +0000

    Update camel_to_snake.c
    update

--- my_answers/exam_rank_02/level_2/camel_to_snake.c/camel_to_snake.c
+++ my_answers/exam_rank_02/level_2/camel_to_snake.c/camel_to_snake.c
@@ -1,60 +1,30 @@
-#include "stdlib.h"
-
-/**
- * Efficient camel_to_snake implementation with: 
- * - Bitwise operations
- * - O(1) time complexity per character
- * - dynamic memory allocation
- */
 #include <unistd.h>
-#include <stdlib.h>
+
+# define NEWLINE() (write(1,"\n",1))
+# define WRITE_CHAR(c) (write(1,&(c),1))
+
+void camel_to_snake(char *str)
+{
+    while (*str)
+    {
+        if (*str >= 'A' && *str <= 'Z')
+        {
+			char underline = '_';
+            WRITE_CHAR(underline);
+            char lower = *str + 32;
+            WRITE_CHAR(lower);
+        }
+        else
+            WRITE_CHAR(*str);
+        str++;
+    }
+    NEWLINE();
+}
 
 int main(int argc, char **argv)
 {
-    char *s;
-    char *result;
-    int len = 0, i = 0, j = 0;
-
     if (argc != 2)
-        return (write(1, "\n", 1), 0);
-    
-    s = argv[1];
-    
-    // Count uppercase letters using bitwise check
-    while (s[len])
-    {
-        // Ensure character is alphabetic and check 6th bit
-        if (((s[len] | 32) >= 'a' && (s[len] | 32) <= 'z') && 
-            ((s[len] & 32) == 0))
-            i++; // Count uppercase letters
-        len++;
-    }
-    
-    result = (char *)malloc(len + i + 1);
-    if (!result)
-        return (write(1, "\n", 1), 0);
-        
-    i = 0;
-    j = 0;
-    
-    while (s[i])
-    {
-        // Detect uppercase using bitwise operation
-        if (((s[i] | 32) >= 'a' && (s[i] | 32) <= 'z') && 
-            ((s[i] & 32) == 0))
-        {
-            result[j++] = '_';
-            result[j++] = s[i] | 32; // Set the 6th bit to convert to lowercase
-        }
-        else
-            result[j++] = s[i];
-        i++;
-    }
-    
-    result[j] = '\0';
-    write(1, result, j);
-    write(1, "\n", 1);
-    
-    free(result);
-    return (0);
+        return (NEWLINE(), 1);
+    camel_to_snake(argv[1]);
+    return 0;
 }
