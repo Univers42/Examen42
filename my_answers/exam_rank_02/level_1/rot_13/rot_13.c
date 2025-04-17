85a99613166267ad86223b6411c7fd7c382a2776
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-17 18:32:15 +0000

    Update rot_13.c
    update

--- my_answers/exam_rank_02/level_1/rot_13/rot_13.c
+++ my_answers/exam_rank_02/level_1/rot_13/rot_13.c
@@ -1,29 +1,32 @@
 #include <unistd.h>
+# define WRITE_NEW_LINE() (write(1,"\n",1))
+// Only bitwise for case detection, no range checks
+# define ISUPPER(c) (((c) & 0x40) && !((c) & 0x20))
+# define ISLOWER(c) (((c) & 0x40) && ((c) & 0x20))
+# define ROT13(c, base, rot) (((c - base + rot) % 26) + base)
+
+void rot_13(char *str)
+{
+    char ch;
+    while(*str)
+    {
+        ch = *str;
+        if (ISUPPER(*str))
+            ch = ROT13(*str, 'A', 13);
+        else if (ISLOWER(*str))
+            ch = ROT13(*str, 'a', 13);
+        write(1, &ch, 1);
+        str++;
+    }
+    WRITE_NEW_LINE();
+}
 
 int main(int argc, char **argv)
 {
     char *str;
-    char c;
-
     if (argc != 2)
-        return (write(1, "\n", 1), 0);
-        
+        return (WRITE_NEW_LINE(), 1);
     str = argv[1];
-    while (*str)
-    {
-        c = *str;
-        if (c & 0x40)
-        {
-            // Apply ROT13 with bitwise modulo
-            // (c - 'A' + 13) % 26
-            if (c >= 'A' && c <= 'Z')
-                c = ((c - 'A' + 13) & 0x1F) + 'A';
-            else if (c >= 'a' && c <= 'z')  // Fixed: added 'if' after else
-                c = ((c - 'a' + 13) & 0x1F) + 'a';  // Fixed: added missing parenthesis
-        }
-        write(1, &c, 1);
-        str++;
-    }
-    write(1, "\n", 1);
+    rot_13(str);
     return (0);
 }
