042bb08eb3443b9c7bef7b0fd42fe76f9237b37c
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-18 13:41:10 +0000

    Update ft_strpbrk.c
    update

--- my_answers/exam_rank_02/level_2/ft_strpbrk.c/ft_strpbrk.c
+++ my_answers/exam_rank_02/level_2/ft_strpbrk.c/ft_strpbrk.c
@@ -19,3 +19,35 @@ char *ft_strpbrk(const char *s1, const char *s2)
     }
     return NULL;
 }
+
+/*
+#include <stdint.h>
+#include <stdio.h>
+
+char *ft_strpbrk(const char *s1, const char *s2)
+{
+    uint32_t bit_map[8] = {0};
+    
+    while(*s2)
+    {
+        bit_map[(unsigned char) *s2 / 32] |= (1U << ((unsigned char)*s2 %32));
+        s2++;
+    }
+    while(*s1)
+    {
+        if (bit_map[(unsigned char) *s1 / 32] & (1U << ((unsigned char)*s1 % 32)))
+            return ((char *)s1);
+        s1++;
+    }
+    return (NULL);
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 2)
+        return 1;
+    char *ret = ft_strpbrk(argv[1], "abcdef");
+    printf("%s\n", ret);
+    return (0);
+}
+*/
