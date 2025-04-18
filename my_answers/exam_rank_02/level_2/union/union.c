2c52aad800d357de6558cdfc7eb5308fcd9a9282
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-18 17:27:07 +0000

    Update union.c
    update

--- my_answers/exam_rank_02/level_2/union/union.c
+++ my_answers/exam_rank_02/level_2/union/union.c
@@ -1,31 +1,49 @@
 #include <unistd.h>
 
+# define BIT_SEQ 8
+# define SET_MAP(bitmap, c) (bitmap[(unsigned char)(c) / 32] |= (1U << (unsigned char)(c) % 32))
+# define CHECK_MAP(bitmap, c) (bitmap[(unsigned char)(c) / 32] & (1U << (unsigned char)(c) % 32))
+# define NEWLINE() (write(1, "\n", 1))
+
+typedef struct s_tables
+{
+    unsigned int seen[BIT_SEQ];
+}t_tables;
+
+void ft_union(char *str1, char *str2)
+{
+    t_tables map = {{0}};
+    
+    while(*str1)
+    {
+        if(!CHECK_MAP(map.seen, *str1))
+        {
+            write(1, str1, 1);
+            SET_MAP(map.seen, *str1);
+        }
+        str1++;
+    }
+    
+    while(*str2)
+    {
+        if(!CHECK_MAP(map.seen, *str2))
+        {
+            write(1, str2, 1);
+            SET_MAP(map.seen, *str2);
+        }
+        str2++;
+    }
+}
+
 int main(int argc, char **argv)
 {
-    unsigned int seen[8] = {0};
-    unsigned char c;
-    char *str;
-    int array_index;
-    int bit_position;
-
-    if (argc != 3)
-        return(write(1, "\n", 1),0);
-    for (int arg = 1; arg < argc; arg++)
+    if(argc != 3)
     {
-        str = argv[arg];
-        while (*str)
-        {
-            c = *str;
-            array_index = c >> 5;       // Divide by 32 to get the index
-            bit_position = c & 0x1F;    //Modulo 32 (c % 32) to get the bit position
-            if (!(seen[array_index] & (1 << bit_position)))
-            {
-                seen[array_index] |= (1 << bit_position);
-                write(1, str, 1);
-            }
-            str++;
-        }
+        NEWLINE();
+        return 0;
     }
-    write(1, "\n", 1);
-    return (0);
+    
+    ft_union(argv[1], argv[2]);
+    NEWLINE();
+    return 0;
 }
