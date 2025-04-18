0c19984bf98266f3bea1563eb178a95baac1ba5f
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-18 15:24:41 +0000

    Update inter.c
    update

--- my_answers/exam_rank_02/level_2/inter/inter.c
+++ my_answers/exam_rank_02/level_2/inter/inter.c
@@ -1,29 +1,40 @@
 #include <unistd.h>
+#include <stdint.h>
+# define WRITE_CHAR(c) (write(1,&(c),1))
+# define WRITE_NEW_LINE(c) (write(1,"\n",1))
+# define BIT_SET(bitmap,c) (bitmap[(unsigned char)(c)/32] |= 1U << (unsigned char)(c) % 32)
+# define BIT_CHECK(bitmap, c) (bitmap[(unsigned char)(c) / 32] & (1U << ((unsigned char)(c) % 32)))
+# define BIT_BUFFER 8
+
+typedef struct s_tables
+{
+    uint32_t seen[BIT_BUFFER];
+    uint32_t printed[BIT_BUFFER];
+}t_tables;
+
+void inter(const char *str1, const char *str2)
+{
+    t_tables table = {{0}, {0}};
+    while(*str2)
+    {
+        BIT_SET(table.seen, *str2);
+        str2++;
+    }
+    while(*str1)
+    {
+        if(BIT_CHECK(table.seen, *str1) && ! BIT_CHECK(table.printed, *str1))
+        {
+            WRITE_CHAR(*str1);
+            BIT_SET(table.printed, *str1);
+        }
+        str1++;
+    }
+}
 
 int main(int argc, char **argv)
 {
-    unsigned char s2_chars[32] = {0};
-    unsigned char used[32] = {0};
-
-    if (argc != 3)
-        return (write(1, "\n", 1), 0);
-    char *s1 = argv[1];
-    char *s2 = argv[2];
-    while(*s2)
-    {
-        s2_chars[*s2 >> 3] |= (1 << (*s2 & 7));
-        s2++;
-    }
-    while(*s1)
-    {
-        if ((s2_chars[*s1 >> 3] & (1 << (*s1 & 7))) && 
-            !(used[*s1 >> 3] & (1 << (*s1 & 7))))
-            {
-                write(1, s1, 1);
-                used[*s1 >> 3] |= (1 << (*s1 & 7));
-            }
-            s1++;
-    }
-    write(1, "\n", 1);
+    if (argc == 3)
+        inter(argv[1], argv[2]);
+    WRITE_NEW_LINE();
     return (0);
 }
