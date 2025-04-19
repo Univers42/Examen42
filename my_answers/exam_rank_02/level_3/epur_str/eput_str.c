ad32b05e878315a7e683b8189c85f3b820e81ceb
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-19 21:10:10 +0000

    Update eput_str.c
    update

--- my_answers/exam_rank_02/level_3/epur_str/eput_str.c
+++ my_answers/exam_rank_02/level_3/epur_str/eput_str.c
@@ -1,37 +1,46 @@
 #include <unistd.h>
 
-// Check if character is whitespace
-#define IS_SPACE(c) ((c) == ' ' || (c) == '\t')
+#define IS_SPACE(c) (((c) ^ ' ') == 0 || ((c) ^ '\t') == 0)
+#define WRITE_CHAR(c) (write(1,&(c),1))
+
+int ft_strlen(char *str)
+{
+    char *temp;
+    temp = str;
+    while(*str)
+        str++;
+    return (str - temp);
+}
+
+//"    hello   world"
+void epur(char *str)
+{
+    char *sentinel;
+    int printed_space = 0;
+    sentinel = str + ft_strlen(str) - 1;
+
+    while (*str && IS_SPACE(*str))
+        str++;
+    while (sentinel > str && IS_SPACE(*sentinel))
+        sentinel--;
+    while (str <= sentinel)
+    {
+        int is_space = IS_SPACE(*str);
+        int should_print = is_space ^ printed_space;
+        if (should_print || !is_space)
+            WRITE_CHAR(*str);
+        printed_space = is_space;
+        str++;
+    }
+    WRITE_CHAR("\n");
+}
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-    {
-        write(1, "\n", 1);
-        return 0;
-    }
-    char *start = argv[1];
-    char *end = argv[1];
-    int word_found = 0;
-    
-    while (*start && IS_SPACE(*start))
-        start++;
-    end = start;
-    while (*end)
-    {
-        while (*end && !IS_SPACE(*end))
-            end++;
-        if (end > start)
-        {
-            if (word_found)
-                write(1, " ", 1);
-            write(1, start, end - start);
-            word_found = 1;
-        }
-        while (*end && IS_SPACE(*end))
-            end++;
-        start = end;
-    }
-    write(1, "\n", 1);
-    return 0;
+    char *str;
+    if(argc != 2)
+        return(WRITE_CHAR("\n"), 1);
+    str = argv[1];
+    epur(str);
+    return (0);
 }
