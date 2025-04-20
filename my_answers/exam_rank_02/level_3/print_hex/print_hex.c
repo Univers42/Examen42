4a4672a330babb6207d03c63216c9b3920f0ab57
dylan <139464836+LESdylan@users.noreply.github.com>
2025-04-20 11:37:27 +0000

    Update print_hex.c
    update

--- my_answers/exam_rank_02/level_3/print_hex/print_hex.c
+++ my_answers/exam_rank_02/level_3/print_hex/print_hex.c
@@ -1,30 +1,38 @@
 #include <unistd.h>
-//recursive approach O(log(n))
+# define NEWLINE() (write(1,"\n",1))
 
-void print_hex(unsigned int n)
+void print_hex_iterative(int number)
 {
     char hex[] = "0123456789abcdef";
-    if (n >= 16)
-        print_hex(n >> 4); //Divide by 16 using bit shift
-    write(1, &hex[n & 0xF], 1);
+    char buffer[16];
+    int  i = 0;
+
+    if (number == 0)
+    {
+        write(1,"0",1);
+        return ;
+    }
+    while(number > 0)
+    {
+        buffer[i++] = hex[number % 16];
+        number /= 16;  
+    }
+    while(i--)
+        write(1,&buffer[i],1);
 }
 
-int main(int argc, char **argv)
+void print_hex(int number)
 {
-    if (argc == 2)
-    {
-        unsigned int num = 0;
-        for (int i = 0; argv[1][i]; i++)
-        {
-            if (argv[1][i] < '0' || argv[1][i] > '9')
-            {
-                write(1, "\n", 1);
-                return (0);
-            }
-            num = (num << 3) + (num << 1) + (argv[1][i] - '0');
-        }
-        print_hex(num);
-    }
-    write(1, "\n", 1);
-    return 0;
+    char hex[] = "0123456789abcdef";
+    if (number >= 16)
+        print_hex(number >> 4); // print_hex(number / 16)
+    write(1, &hex[number & 0xF], 1); //hex[number % 16]
+}
+
+int main(void)
+{
+    int number = 67;
+    print_hex(number);
+    NEWLINE();
+    return (0);
 }
