35f3b265e30e1edbfc1a8a01151ab78f508b05a3
LESdylan <dev.pro.photo@gmail.com>
2025-09-20 13:02:43 +0200

    update

--- Exam_Rank_03/n_queens/solutions.c
+++ Exam_Rank_03/n_queens/solutions.c
@@ -4,43 +4,19 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-#define MAX_CANDIDATE 100
-
 bool		is_solution(int n, int k)
 {
 	return(k == n);
 }
 
-#include <unistd.h>
-#include <stdio.h>
-
 void print_solution(int *a, int n)
 {
-    int i = 0;
-    char buf[12];
-    while (i < n)
-    {
-        int num = a[i];
-        int len = 0;
-        if (num == 0)
-            buf[len++] = '0';
-        else
-        {
-            int tmp = num, digits[12], d = 0;
-            while (tmp > 0)
-            {
-                digits[d++] = tmp % 10;
-                tmp /= 10;
-            }
-            while (d-- > 0)
-                buf[len++] = '0' + digits[d];
-        }
-        write(1, buf, len);
+    for (int i = 0; i < n; i++) {
+        fprintf(stdout, "%d", a[i]);
         if (i < n - 1)
-            write(1, " ", 1);
-        i++;
+            fprintf(stdout, " ");
     }
-    write(1, "\n", 1);
+    fprintf(stdout, "\n");
 }
 
 bool is_safe(int *a, int k, int candidate)
@@ -109,7 +85,7 @@ int main(int argc, char **argv)
     if (argc != 2)
         return 1;
     n = atoi(argv[1]);
-    if (n <= 0 || n > MAX_CANDIDATE)
+    if (n <= 0)
         return 1;
     a = malloc(sizeof(int) * n);
     if (!a)
