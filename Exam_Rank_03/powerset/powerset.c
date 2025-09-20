9b5e0ef620749d3f1e47dbff5071993b3a78f480
LESdylan <dev.pro.photo@gmail.com>
2025-09-20 15:34:56 +0200

    update

--- Exam_Rank_03/powerset/powerset.c
+++ Exam_Rank_03/powerset/powerset.c
@@ -1,107 +1,124 @@
-#include "powerset.h"
-#include <stdio.h>
-#include <string.h>
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   powerset.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dlesieur <dlesieur@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 15:34:49 by dlesieur          #+#    #+#             */
+/*   Updated: 2025/09/20 15:34:50 by dlesieur         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <unistd.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-# define RECLVL 0
-# define MAX_CANDIDATE 2
-/**
- * why use k as the limit ?
- * at each recursion level, we are building a partial solution of length `k`
- * The array `a` holds  the current subset ( or choices) up to depth `k`
- * so when checking if we have a solution, we want ot sum the element that have been decided so far
- * if we have 5 numbers but are at the recursion depth `k = 3`
- * only the first 3 choices in `a`are meaningfulñ at this recursion depth
- */
-bool is_a_solution(int *a, int *arr, int k, int target)
+# define MAX_CANDIDATES 2
+
+bool	is_solution(int *a, int *nums, int n, int target)
 {
-    int sum = 0;
-    for (int i = 0; i < k; i++)
-        if (a[i])
-            sum += arr[i];
-    return (sum == target);
+	int i;
+	int	res;
+
+	i = 0;
+	res = 0;
+	while (i < n)
+	{
+		if (a[i])
+			res += nums[i];
+		i++;
+	}
+	if (res == target)
+		return (true);
+	return (false);
 }
 
-
-void print_solution(int *a, int *arr, int k)
+void	print_solution(int *a, int *nums, int n)
 {
 	int	i;
 
-	i = -1;
-    printf("{ ");
-    while (++i < k)
-        if (a[i])
-            printf("%d ", arr[i]);
-    printf("}\n");
+	i = 0;
+	while (i < n)
+	{
+		if (a[i])
+			printf("%d ", nums[i]);
+		i++;
+	}
+	printf("\n");
 }
 
-
-void build_candidate(int *c, int *nc)
+void	build_candidate(int *c, int *nc)
 {
-	c[0] = 1; //include
-	c[1] = 0; //exclude
+	c[0] = 1;
+	c[1] = 0;
 	*nc = 2;
 }
 
-void make_move(int *a, int k, int value)
+void	make_move(int *a, int k, int  value)
 {
 	a[k] = value;
 }
 
 void	unmake_move(int *a, int k, int value)
 {
-	(void)a,
+	(void)a;
 	(void)k;
 	(void)value;
 }
 
-void powerset(int *a, int *arr, int k, int n, int target)
+void    powerset(int *a, int *nums, int k, int n, int target)
 {
-    int candidate[MAX_CANDIDATE];
-    int nc;
-	int	i;
-    int new_k;
+	int candidates[2];
+	int nc;
+	int i;
 
-    if (k == n)
-    {
-        if (is_a_solution(a, arr, k, target))
-            print_solution(a, arr, k);
-    }
-    else
-    {
-        build_candidate(candidate, &nc);
-        new_k = k + 1;
-        i = -1;
-        while (++i < nc)
-        {
-            make_move(a, k, candidate[i]);
-            powerset(a, arr, new_k , n, target);
-            unmake_move(a, k, candidate[i]);
-        }
-    }
+	// Base case: when we've decided for all elements, check and return
+	if (k == n)
+	{
+		if (is_solution(a, nums, n, target))
+			print_solution(a, nums, n);
+		return;
+	}
+	else
+	{
+		build_candidate(candidates, &nc);
+		i = -1;
+		while (++i < nc)
+		{
+			make_move(a, k, candidates[i]);
+			powerset(a, nums, k + 1, n, target);
+			unmake_move(a, k, candidates[i]);
+		}
+	}
 }
 
 int main(int argc, char **argv)
 {
-    int *arr;
-    int *a;
-    int target;
-    int n;
+	int	*a;
+	int	*nums;
+	int	n;
+	int	target;
 	int	i;
+	int	k;
 
-    if (argc < 3)
-        return (1);
-    target = atoi(argv[1]);
-    n = argc - 2;
-    arr = calloc(n, sizeof(int));
-    a = calloc(n, sizeof(int));
+	if (argc == 1)
+		return (1);
+	n = argc - 2;
+	target = atoi(argv[1]);
+	a = calloc(n, sizeof(int));
+	if (!a)
+		return (1);
+	nums = calloc(n, sizeof(int));
+	if (!nums)
+		return (free(a), 1);
 	i = -1;
-    while (++i < n)
-        arr[i] = atoi(argv[i + 2]);
-    powerset(a, arr, RECLVL, n, target);
-    free(arr);
-    free(a);
-    return (0);
+	k = 0;
+	while (++i < n)
+		nums[i] = atoi(argv[i + 2]);
+	powerset(a, nums, k, n, target);
+	return (free(a), free(nums), 0);
 }
