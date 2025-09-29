2e907c9394c1d58825d7d40a8555026c5eb589fd
LESdylan <dev.pro.photo@gmail.com>
2025-09-29 15:24:44 +0200

    update

--- Exam_Rank_03/n_queens/training.c
+++ Exam_Rank_03/n_queens/training.c
@@ -1,178 +1,95 @@
 #include <unistd.h>
-#include <stdio.h>
 #include <string.h>
+#include <stdio.h>
 #include <stdlib.h>
-<<<<<<< Updated upstream
+#include <fcntl.h>
 #include <stdbool.h>
 
-# define MAX_CANDIDATES 30
 
 void	print_solution(int *a, int n)
 {
-	static	bool solved = false;
 	int	i;
-	int	first = 0;
+	int	first;
 
 	i = -1;
+	first = 1;
 	while (++i < n)
 	{
-		if (first)
-			fprintf(stdout, " ");
-		fprintf(stdout, "%d", a[i]);
-		solved = true;
-		first = 1;
+		if (!first)
+			printf(" ");
+		printf("%d", a[i]);
+		first = 0;
 	}
-	if (first)
-		fprintf(stdout, "\n");
-	if (!solved && n == 0)
-		fprintf(stdout,  "\n");
+	printf("\n");
 }
 
-bool	is_safe(int *a, int col, int k)
-=======
-#include <stdlib.h>
-#include <stdbool.h>
-
-void print_solution(int *a, int n)
-{
-    int i;
-
-    i = -1;
-    while (++i < n)
-    {
-        fprintf(stdout, "%d", a[i]);
-        if (i < n - 1)
-            fprintf(stdout, " ");
-    }
-    fprintf(stdout, "\n");
-}
-
-bool    is_safe(int *a, int k, int col)
->>>>>>> Stashed changes
+bool	is_safe(int *a, int k, int col)
 {
 	int	i;
 
-<<<<<<< Updated upstream
-	i = -1;
+	i = -1;	//i maybe has to be equal to K
 	while (++i < k)
 	{
-		if(a[i] == col || a[i] - i == col - k|| a[i] + i == col + k)
+		if (a[i] == col || a[i] + i == col + k || a[i] - i == col - k)
 			return (false);
 	}
-	return  (true);
+	return (true);
 }
 
-void	build_candidate(int *c, int *nc, int *a, int k, int n)
+void	build_candidate(int *candidate, int *nc, int k, int *a, int n)
 {
 	int	col;
 
-	col = -1;
 	*nc = 0;
+	col = -1;
 	while (++col < n)
 	{
-		if (is_safe(a, col, k))
+		if (is_safe(a, k, col))
 		{
-			c[*nc] = col;
+			candidate[*nc] = col;
 			(*nc)++;
 		}
 	}
-=======
-    i = -1;
-    while (++i < k)     //maybe k, maybe nc
-    {
-        if (a[i] == col || a[i] - i == col - k || a[i] + i == col + k)
-            return (false);
-    }
-    return (true);
 }
 
-void build_candidate(int *c, int *nc, int *a, int k, int n)
+void	n_queen(int *a, int k, int n)
 {
-    int     col;
-
-    col = -1;
-    *nc = 0;
-    while (++col < n)
-    {
-        if (is_safe(a, k, col))
-        {
-            c[*nc] = col;
-            (*nc)++;
-        }
-    }
->>>>>>> Stashed changes
-}
-
-void    n_queen(int *a, int k, int n)
-{
-<<<<<<< Updated upstream
-	int	candidate[MAX_CANDIDATES];
+	int	*candidate;
 	int	nc;
 	int	i;
 
+	candidate = calloc(n, sizeof(int));
+	if (!candidate)
+		return ;
 	if (k == n)
 		print_solution(a, n);
 	else
 	{
+		build_candidate(candidate, &nc, k, a ,n);
 		i = -1;
-		build_candidate(candidate, &nc, a, k, n);
 		while (++i < nc)
 		{
 			a[k] = candidate[i];
 			n_queen(a, k + 1, n);
 		}
 	}
-=======
-    int *candidate;
-    int nc;
-    int i;
-
-    candidate = calloc(n, sizeof(int));
-    if (!candidate)
-        return ;
-    if (n == k)
-        print_solution(a, n);
-    else
-    {
-        i = -1;
-        build_candidate(candidate, &nc, a, k, n);
-        while (++i < nc)
-        {
-            a[k] = candidate[i];
-            n_queen(a, k + 1, n);
-        }
-    }
-    free(candidate);
->>>>>>> Stashed changes
+	free(candidate);
 }
 
 int main(int argc, char **argv)
 {
-<<<<<<< Updated upstream
-	int k;
-	int n;
+	int *a;
+	int	n;
+	int	k;
 
 	if (argc != 2)
 		return (1);
 	n = atoi(argv[1]);
-	int	a[n];
+	a = calloc(sizeof(int), n);
+	if (!a)
+		return (1);
 	k = 0;
 	n_queen(a, k, n);
-	print_solution(a, 0);
+	free(a);
 	return (0);
 }
-=======
-    int n;
-    int *a;
-    int k;
-
-    n = atoi(argv[1]);
-    a = calloc(n, sizeof(int));
-    if (!a)
-        return (1);
-    k = 0;
-    n_queen(a, k, n);
-    free(a);
-    return (0);
-}
->>>>>>> Stashed changes
--- Exam_Rank_03/permutation/training.c
+++ Exam_Rank_03/permutation/training.c
@@ -1,36 +1,27 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-void    ft_swap(void *a, void *b, size_t n)
+void    ft_swap(void *ptr1, void *ptr2, size_t n)
 {
     unsigned char *pa;
     unsigned char *pb;
 
-    if (a == b)
+    pa = (unsigned char *)ptr1;
+    pb = (unsigned char *)ptr2;
+    if (pa == pb)
         return ;
-    pa = (unsigned char *)a;
-    pb = (unsigned char *)b;
     while (n--)
         (*pa ^= *pb), (*pb ^= *pa), (*pa++ ^= *pb++);
 }
 
-<<<<<<< Updated upstream
 void    selection_sort(char *pattern, int *c, int *nc)
 {
     int i;
     int j;
 
-=======
-void    selection_sort(int *c, int *nc, char *pattern, int k)
-{
-    int     i;
-    int     j;
-
-    (void)k;
->>>>>>> Stashed changes
     i = -1;
     while (++i < *nc - 1)
     {
@@ -43,114 +34,55 @@ void    selection_sort(int *c, int *nc, char *pattern, int k)
     }
 }
 
-<<<<<<< Updated upstream
 void    build_candidate(char *pattern, int *c, int *nc, int n, int k)
-=======
-void    build_candidate(char *pattern, int *c, int *nc, int k, int n)
->>>>>>> Stashed changes
 {
     int i;
 
-    i = k;
     *nc = 0;
-<<<<<<< Updated upstream
-    while(i < n)
-=======
+    i = k;
     while (i < n)
->>>>>>> Stashed changes
     {
         c[*nc] = i;
         (*nc)++;
         i++;
     }
-<<<<<<< Updated upstream
     selection_sort(pattern, c, nc);
 }
 
-void    permutation(char *pattern, int *a, int k, int n)
-=======
-    selection_sort(c, nc, pattern, k);
-}
-
 void    permutation(char *pattern, int k, int n)
->>>>>>> Stashed changes
 {
-    int *candidate;
+    int candidate[n];
     int nc;
     int i;
     int idx;
 
-    candidate = calloc(n, sizeof(int));
-<<<<<<< Updated upstream
-    if (!candidate)
-        return ;
     if (k == n)
         puts(pattern);
     else
     {
-        build_candidate(pattern, candidate, &nc, n, k);
         i = -1;
+        build_candidate(pattern, candidate, &nc, n, k);
         while (++i < nc)
         {
             idx = candidate[i];
-            ft_swap(&pattern[idx], &pattern[k],  sizeof(char));
-            permutation(pattern, a, k + 1, n);
+            ft_swap(&pattern[idx], &pattern[k], sizeof(char));
+            permutation(pattern, k + 1, n);
             ft_swap(&pattern[idx], &pattern[k], sizeof(char));
         }
     }
-    free(candidate);
 }
-
-int main(int argc, char **argv)
-{
-    char    *pattern;
-    int     *a;
-    int     k;
-    int     n;
-
-    if (argc != 2)
-        return (1);
-    pattern = argv[1];
-    n = strlen(pattern);
-    a = calloc(n, sizeof(int));
-    if (!a)
-        return (1);
-    pattern = argv[1];
-    k = 0;
-    permutation(pattern, a, k, n);
-    free(a);
-    return (0);
-}
-=======
-    if (n == k)
-        puts(pattern);
-    else
-    {
-        idx = -1;
-        build_candidate(pattern, candidate, &nc, k, n);
-        while (++idx < nc)
-        {
-            i = candidate[idx];
-            ft_swap(&pattern[i], &pattern[k], sizeof(char));
-            permutation(pattern, k + 1, n);
-            ft_swap(&pattern[i], &pattern[k], sizeof(char));
-        }
-    }
-    free(candidate);
-}   
 
 int main(int argc, char **argv)
 {
     char *pattern;
-    int n;
     int k;
+    int n;
 
     if (argc != 2)
         return (1);
-    k = 0;
     pattern = argv[1];
     n = strlen(pattern);
+    k = 0;
     permutation(pattern, k, n);
     return (0);
 }
->>>>>>> Stashed changes
