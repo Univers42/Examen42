0c9ae8ae4339702cd6529ec8f2e6613c2ced504d
Dylan Lesieur <dlesieur@c3r11s4.42madrid.com>
2025-10-01 21:34:54 +0200

    udpate

--- Exam_Rank_03/rip/solution.c
+++ Exam_Rank_03/rip/solution.c
@@ -1,5 +1,8 @@
-#include "exam.h"
-// only puts and write I can use
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
 int	ft_strlen(const char *src)
 {
