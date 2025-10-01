3e68e6d6bc5d318c256899cfe4c1afa0457de4ba
Dylan Lesieur <dlesieur@c3r11s4.42madrid.com>
2025-10-01 21:33:47 +0200

    update

--- Exam_Rank_03/rip/solution.c
+++ Exam_Rank_03/rip/solution.c
@@ -1,124 +1,115 @@
-#include <unistd.h>
+#include "exam.h"
+// only puts and write I can use
 
-#define MAX_LEN 4096
-
-static int		ft_strlen(const char *s);
-static void		print_solution(char *buf, int n);
-static void		compute_removals(const char *s, int n, int *lr, int *rr);
-static void		dfs_build(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr);
-static void		rec_open(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr);
-static void		rec_close(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr);
-
-static int	ft_strlen(const char *s)
+int	ft_strlen(const char *src)
 {
-	int	i;
+	const char *tmp = src;
 
-	i = 0;
-	while (s[i])
-		i++;
-	return (i);
-}
-static void	print_solution(char *buf, int n)
-{
-	write(1, buf, n);
-	write(1, "\n", 1);
+	while (*tmp++)
+		;
+	return  (tmp - src - 1);
 }
 
-
-static void	compute_removals(const char *s, int n, int *lr, int *rr)
+void	compute_removals(const char *src, int *lr,  int *rr, int n)
 {
 	int	i;
 	int	bal;
 
-	i = 0;
-	bal = 0;
 	*lr = 0;
 	*rr = 0;
-	while (i < n)
+	i = -1;
+	bal = 0;
+	while (++i < n)
 	{
-		if (s[i] == '(')
+		if (src[i] == '(')
 			bal++;
-		else if (s[i] == ')')
+		else if (src[i] ==  ')')
 		{
 			if (bal == 0)
 				(*rr)++;
 			else
 				bal--;
 		}
-		i++;
 	}
 	*lr = bal;
 }
 
-static void	rec_open(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr)
-{
-	cur[i] = '(';
-	dfs_build(src, cur, n, i + 1, bal + 1, lr, rr);
+static void	dfs_build(char *src, char *cur, int k, int n, int bal, int lr,  int rr);
+
+void	rec_open(char *src, char *cur, int k, int n, int bal, int lr,  int rr)
+{	
+	cur[k] = '(';
+	dfs_build(src, cur, k + 1, n, bal + 1, lr, rr);
 	if (lr > 0)
 	{
-		cur[i] = ' ';
-		dfs_build(src, cur, n, i + 1, bal, lr - 1, rr);
+		cur[k] = ' ';
+		dfs_build(src, cur, k + 1, n, bal, lr - 1, rr);
 	}
 }
 
-static void	rec_close(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr)
+void	rec_close(char *src, char *cur, int k, int n, int bal, int lr,  int rr)
 {
 	if (bal > 0)
 	{
-		cur[i] = ')';
-		dfs_build(src, cur, n, i + 1, bal - 1, lr, rr);
+		cur[k] = ')';
+		dfs_build(src, cur, k + 1, n, bal - 1, lr, rr);
 	}
 	if (rr > 0)
 	{
-		cur[i] = ' ';
-		dfs_build(src, cur, n, i + 1, bal, lr, rr - 1);
+		cur[k] = ' ';
+		dfs_build(src, cur, k + 1, n, bal, lr, rr - 1);
 	}
 }
 
-static void	dfs_build(const char *src, char *cur, int n, int i,
-					int bal, int lr, int rr)
+static void	dfs_build(char *src, char *cur, int k, int n, int bal, int lr,  int rr)
 {
 	char	c;
 
-	if (i == n)
+	if (k == n)
 	{
 		if (bal == 0 && lr == 0 && rr == 0)
-			print_solution(cur, n);
-		return ;
+		{
+			write(STDOUT_FILENO, cur, n);
+			write(STDOUT_FILENO, "\n", 1);
+		}
 	}
-	c = src[i];
-	if (c == '(')
-		rec_open(src, cur, n, i, bal, lr, rr);
-	else if (c == ')')
-		rec_close(src, cur, n, i, bal, lr, rr);
 	else
 	{
-		cur[i] = c;
-		dfs_build(src, cur, n, i + 1, bal, lr, rr);
+		c = src[k];
+		if (c == '(')
+			rec_open(src, cur, k, n, bal, lr, rr);
+		else if (c == ')')
+			rec_close(src, cur, k, n, bal, lr, rr);
+		else
+		{
+			cur[k] = c;
+			dfs_build(src, cur, k + 1, n, bal, lr, rr);
+		}
 	}
 }
 
 int	main(int argc, char **argv)
 {
+	char	*src;
+	int		k;
 	int		n;
-	int		lr;	//left removal
-	int		rr;	//right removal -- number of extra ')' parentehses to remove
-	char	buf[MAX_LEN];
+	int		bal;
+	int		lr;
+	int		rr;
+	int		i;
 
 	if (argc != 2)
-	{
-		write(1, "\n", 1);
-		return (0);
-	}
-	n = ft_strlen(argv[1]);
-	if (n > MAX_LEN)
-		n = MAX_LEN;
-	compute_removals(argv[1], n, &lr, &rr);
-	dfs_build(argv[1], buf, n, 0, 0, lr, rr);
+		return (1);
+	src = argv[1];
+	n = ft_strlen(src);
+	src = argv[1];
+	char	cur[n];
+	i = -1;
+	while (++i < n)
+		cur[i] = src[i];
+	k = 0;
+	bal = 0;
+	compute_removals(src, &lr, &rr, n);
+	dfs_build(src, cur, k, n, bal, lr, rr);
 	return (0);
 }
